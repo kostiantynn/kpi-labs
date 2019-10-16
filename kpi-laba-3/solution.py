f4d2e773fc782db6299eaef8cb5e909b39fbf202
Kostiantyn <kostya.code@gmail.com>
2019-10-16 23:24:20 +0300

    Solution without break + rename variables

--- kpi-laba-3/solution.py
+++ kpi-laba-3/solution.py
@@ -1,10 +1,12 @@
 def main():
 
-    xn_0 = 1
-    while True:
-        xn = (2-xn_0**3)/5
+    condition = True
+    previous = 1
+    while condition:
+        current = (2-previous**3)/5
 
-        if abs(xn - xn_0) < 10**(-5):
-            return xn
-            break
-        else: xn_0 = xn
+        if abs(current - previous) < 10**(-5):
+            condition = False
+            return current
+
+        else: previous = current
