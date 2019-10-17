adfd8126a8ad31e081bcbf704083e2026d026355
Kostiantyn <kostya.code@gmail.com>
2019-10-17 22:26:29 +0300

    Executing main()

--- kpi-laba-3/solution.py
+++ kpi-laba-3/solution.py
@@ -1,12 +1,10 @@
 def main():
-
-    condition = True
     previous = 1
-    while condition:
+    while True:
         current = (2-previous**3)/5
-
         if abs(current - previous) < 10**(-5):
-            condition = False
             return current
-
         else: previous = current
+# executing main() function
+if __name__ == '__main__':
+    main()
