873d5f50c950a9baa1f5051ce614024a4a81dd97
Kostiantyn <kostya.code@gmail.com>
2019-10-29 16:38:29 +0200

    simplify factorial

--- kpi-laba-4/solution.py
+++ kpi-laba-4/solution.py
@@ -1,11 +1,12 @@
-from math import factorial
-
 def main():
     d = int(input('Enter n: '))
     n = 1
     Sum = 0
+    factorial = 1
     while n <= d:
-        Sum += 1/factorial(n)
+
+        Sum += 1/factorial
+        factorial = factorial * n
         n+=1
     return Sum
 
