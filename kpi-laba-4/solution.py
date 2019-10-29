de76175ba81e85b00dba366e170320f940414928
Kostiantyn <kostya.code@gmail.com>
2019-10-29 16:44:35 +0200

    simplify factorial operation

--- kpi-laba-4/solution.py
+++ kpi-laba-4/solution.py
@@ -2,11 +2,11 @@ def main():
     d = int(input('Enter n: '))
     n = 1
     Sum = 0
-    factorial = 1
+    factorial_var = 1
     while n <= d:
 
-        Sum += 1/factorial
-        factorial = factorial * n
+        Sum += 1/factorial_var
+        factorial_var = factorial_var * n
         n+=1
     return Sum
 
